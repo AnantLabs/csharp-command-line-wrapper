65b5ee8dff3fca7d26bdf77d5ea31e80f01b21e5
glassware1975@gmail.com <glassware1975@gmail.com@56ec3158-b9d2-a39f-6cf4-0d7476896d1e>
2012-08-08 18:43:06 +0000

    Merged the DocsByReflection code into the single CS file so that users can simply drop CommandWrapLib.cs into their program without adding any dependencies.

--- trunk/CommandWrapper/CommandWrapLib/CommandWrapLib.cs
+++ trunk/CommandWrapper/CommandWrapLib/CommandWrapLib.cs
@@ -2,6 +2,14 @@
  * 2012 Ted Spence, http://tedspence.com
  * License: http://www.apache.org/licenses/LICENSE-2.0 
  * Home page: https://code.google.com/p/csharp-command-line-wrapper
+
+ * Some portions retrieved from DocsByReflection by Jim Blackler: http://jimblackler.net/blog/?p=49
+ * His copyright notice is:
+//Except where stated all code and programs in this project are the copyright of Jim Blackler, 2008.
+//jimblackler@gmail.com
+//
+//This is free software. Libraries and programs are distributed under the terms of the GNU Lesser
+//General Public License. Please see the files COPYING and COPYING.LESSER.
  */
 using System;
 using System.Collections.Generic;
@@ -9,6 +17,7 @@ using System.Linq;
 using System.Text;
 using System.Reflection;
 using System.Xml;
+using System.IO;
 
 namespace CommandWrapLib
 {
@@ -144,7 +153,7 @@ namespace CommandWrapLib
             // Is it possible to get some documentation?
             XmlElement documentation = null;
             try {
-                documentation = DocsByReflection.XMLFromMember(m);
+                documentation = XMLFromMember(m);
             } catch {
                 System.Diagnostics.Debug.WriteLine("XML Help is not available.  Please compile your program with XML documentation turned on if you wish to use XML documentation.");
             }
@@ -199,6 +208,159 @@ namespace CommandWrapLib
                 return -1;
             } else {
                 return 0;
+            }
+        }
+        #endregion
+
+        #region Jim Blackler's Docs By Reflection code, added here to make copying and pasting this code easier
+        /// <summary>
+        /// Provides the documentation comments for a specific method
+        /// </summary>
+        /// <param name="methodInfo">The MethodInfo (reflection data ) of the member to find documentation for</param>
+        /// <returns>The XML fragment describing the method</returns>
+        public static XmlElement XMLFromMember(MethodInfo methodInfo)
+        {
+            // Calculate the parameter string as this is in the member name in the XML
+            string parametersString = "";
+            foreach (ParameterInfo parameterInfo in methodInfo.GetParameters()) {
+                if (parametersString.Length > 0) {
+                    parametersString += ",";
+                }
+
+                parametersString += parameterInfo.ParameterType.FullName;
+            }
+
+            //AL: 15.04.2008 ==> BUG-FIX remove “()” if parametersString is empty
+            if (parametersString.Length > 0) {
+                return XMLFromName(methodInfo.DeclaringType, 'M', methodInfo.Name + "(" + parametersString + ")");
+            } else {
+                return XMLFromName(methodInfo.DeclaringType, 'M', methodInfo.Name);
+            }
+        }
+
+        /// <summary>
+        /// Provides the documentation comments for a specific member
+        /// </summary>
+        /// <param name="memberInfo">The MemberInfo (reflection data) or the member to find documentation for</param>
+        /// <returns>The XML fragment describing the member</returns>
+        public static XmlElement XMLFromMember(MemberInfo memberInfo)
+        {
+            // First character [0] of member type is prefix character in the name in the XML
+            return XMLFromName(memberInfo.DeclaringType, memberInfo.MemberType.ToString()[0], memberInfo.Name);
+        }
+
+        /// <summary>
+        /// Provides the documentation comments for a specific type
+        /// </summary>
+        /// <param name="type">Type to find the documentation for</param>
+        /// <returns>The XML fragment that describes the type</returns>
+        public static XmlElement XMLFromType(Type type)
+        {
+            // Prefix in type names is T
+            return XMLFromName(type, 'T', "");
+        }
+
+        /// <summary>
+        /// Obtains the XML Element that describes a reflection element by searching the 
+        /// members for a member that has a name that describes the element.
+        /// </summary>
+        /// <param name="type">The type or parent type, used to fetch the assembly</param>
+        /// <param name="prefix">The prefix as seen in the name attribute in the documentation XML</param>
+        /// <param name="name">Where relevant, the full name qualifier for the element</param>
+        /// <returns>The member that has a name that describes the specified reflection element</returns>
+        private static XmlElement XMLFromName(Type type, char prefix, string name)
+        {
+            string fullName;
+
+            if (String.IsNullOrEmpty(name)) {
+                fullName = prefix + ":" + type.FullName;
+            } else {
+                fullName = prefix + ":" + type.FullName + "." + name;
+            }
+
+            XmlDocument xmlDocument = XMLFromAssembly(type.Assembly);
+
+            XmlElement matchedElement = null;
+
+            foreach (XmlElement xmlElement in xmlDocument["doc"]["members"]) {
+                if (xmlElement.Attributes["name"].Value.Equals(fullName)) {
+                    if (matchedElement != null) {
+                        throw new Exception("Multiple matches to query");
+                    }
+
+                    matchedElement = xmlElement;
+                }
+            }
+
+            if (matchedElement == null) {
+                throw new Exception("Could not find documentation for specified element");
+            }
+
+            return matchedElement;
+        }
+
+        /// <summary>
+        /// A cache used to remember Xml documentation for assemblies
+        /// </summary>
+        static Dictionary<Assembly, XmlDocument> cache = new Dictionary<Assembly, XmlDocument>();
+
+        /// <summary>
+        /// A cache used to store failure exceptions for assembly lookups
+        /// </summary>
+        static Dictionary<Assembly, Exception> failCache = new Dictionary<Assembly, Exception>();
+
+        /// <summary>
+        /// Obtains the documentation file for the specified assembly
+        /// </summary>
+        /// <param name="assembly">The assembly to find the XML document for</param>
+        /// <returns>The XML document</returns>
+        /// <remarks>This version uses a cache to preserve the assemblies, so that 
+        /// the XML file is not loaded and parsed on every single lookup</remarks>
+        public static XmlDocument XMLFromAssembly(Assembly assembly)
+        {
+            if (failCache.ContainsKey(assembly)) {
+                throw failCache[assembly];
+            }
+
+            try {
+
+                if (!cache.ContainsKey(assembly)) {
+                    // load the docuemnt into the cache
+                    cache[assembly] = XMLFromAssemblyNonCached(assembly);
+                }
+
+                return cache[assembly];
+            } catch (Exception exception) {
+                failCache[assembly] = exception;
+                throw exception;
+            }
+        }
+
+        /// <summary>
+        /// Loads and parses the documentation file for the specified assembly
+        /// </summary>
+        /// <param name="assembly">The assembly to find the XML document for</param>
+        /// <returns>The XML document</returns>
+        private static XmlDocument XMLFromAssemblyNonCached(Assembly assembly)
+        {
+            string assemblyFilename = assembly.CodeBase;
+
+            const string prefix = "file:///";
+
+            if (assemblyFilename.StartsWith(prefix)) {
+                StreamReader streamReader;
+
+                try {
+                    streamReader = new StreamReader(Path.ChangeExtension(assemblyFilename.Substring(prefix.Length), ".xml"));
+                } catch (FileNotFoundException exception) {
+                    throw new Exception("XML documentation not present (make sure it is turned on in project properties when building)", exception);
+                }
+
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(streamReader);
+                return xmlDocument;
+            } else {
+                throw new Exception("Could not ascertain assembly filename");
             }
         }
         #endregion
